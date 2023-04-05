ed7f70acbf61240a9a2fc435cdaa71a89c7d5ccf
oagle.harshal <harshal.ogale@flentas.com>
2023-04-05 23:50:13 +0530

    conditional styling to the input box

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,27 +1,45 @@
-import React, { useState } from 'react';
+/** @format */
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import React, { useState } from "react";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-  const goalInputChangeHandler = event => {
+const CourseInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isValid, setIsValid] = useState(true);
+  const [inputValue, setInputValue] = useState();
+
+  const goalInputChangeHandler = (event) => {
+    event.preventDefault();
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
+    setInputValue("");
   };
 
-  const formSubmitHandler = event => {
+  const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className='form-control'>
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <input
+          // value={inputValue}
+          type='text'
+          onChange={goalInputChangeHandler}
+          style={{ border: !isValid ? "2px solid red" : "2px solid green" }}
+        />
       </div>
-      <Button type="submit">Add Goal</Button>
+      <Button type='submit'>Add Goal</Button>
     </form>
   );
 };
