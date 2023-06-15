eaf2b757e33432838240e1674165e14151477df2
harshal <harshal.ogale@flentas.com>
2023-06-15 13:43:00 +0530

    pratice changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 /** @format */
 
 import "./App.css";
+import AddUser from "./Component/Users/AddUser";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className="App">
+      <AddUser> </AddUser>
+    </div>
+  );
 }
 
 export default App;
